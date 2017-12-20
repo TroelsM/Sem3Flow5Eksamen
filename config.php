adf57a82bdd2622b68da30d9d10f52cf3b5229e5
TroelsM <Troels@bj59.dk>
2017-12-20 08:45:06 +0100

    d
    d

--- config.php
+++ config.php
@@ -1,15 +1,11 @@
 <?php
 session_start();
 
-$DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = 'root';
-$DB_NAME = 'flow5';
 
-//$DB_HOST = 'troelsm.com.mysql';
-//$DB_USER = 'troelsm_com';
-//$DB_PASS = '';
-//$DB_NAME = 'troelsm_com';
+$DB_HOST = 'troelsm.com.mysql';
+$DB_USER = 'troelsm_com';
+$DB_PASS = 'hGdsaAB';
+$DB_NAME = 'troelsm_com';
 
 $link = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($link->connect_error) {
